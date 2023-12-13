1d8c8dabb6eeb9dfb5f1b285198f548a7cec6ac6
prajwol <shresthaprajwol161@gmail.com>
2023-12-14 00:00:13 +0545

    basics of backend has been learned using express

---
+++ 1.learn what matters/script.js
@@ -0,0 +1,24 @@
+let value=require("./script2");
+console.log(value);
+
+// const express = require('express')
+// const app = express()
+
+// app.use(function(req,res,next){
+//     console.log("hello form middleware");
+//     next();
+// })
+// app.use(function(req,res,next){
+//     console.log("hello form middleware1234");
+//     next();
+// })
+
+// app.get('/', function (req, res) {
+//   res.send("hello hi and bye lalalal jingala lalal huhu:)")
+// })
+
+// app.get('/profile', function (req, res) {
+//     res.send("hello from profile:)")
+//   })
+  
+// app.listen(3000);
---
+++ 1.learn what matters/script2.js
@@ -0,0 +1,18 @@
+// let p=17;
+// let y=29;
+// module.exports={first:p, second:y};
+
+// var oneLinerJoke = require('one-liner-joke');
+// var getRandomJoke = oneLinerJoke.getRandomJoke();
+// console.log(getRandomJoke)
+
+// var figlet = require("figlet");
+
+// figlet("Prajwol", function (err, data) {
+//   if (err) {
+//     console.log("Something went wrong...");
+//     console.dir(err);
+//     return;
+//   }
+//   console.log(data);
+// });
